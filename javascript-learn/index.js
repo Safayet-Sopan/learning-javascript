11ab4028febb6603a035cfe05473d8b3878f7c50
Safayet-Sopan <safayetgazisopan@gmail.com>
2026-03-20 13:52:07 +0600

    Minor change on age variable

--- javascript-learn/index.js
+++ javascript-learn/index.js
@@ -9,18 +9,22 @@
 // const ifInput = document.getElementById("ifInput");
 // const ifBtn = document.getElementById("ifBtn");
 // const mainText = document.getElementById("mainText");
+// let age = 0;
 
 // ifBtn.onclick = function(){
-//     if (ifInput.value < 0) {
+//     age = ifInput.value;
+//     age = Number(age);
+
+//     if (age < 0) {
 //         mainText.textContent = `Age is invalid! Try Again!`;
 //     }
-//     else if (ifInput.value == 0) {
+//     else if (age == 0) {
 //         mainText.textContent = `Bro you haven't born yet -_-`;
 //     }
-//     else if (ifInput.value > 0 && ifInput.value < 18) {
+//     else if (age > 0 && ifInput.value < 18) {
 //         mainText.textContent = `You're a child bruv!!! GROW UPP`;
 //     }
-//     else if(ifInput.value > 18 && ifInput.value < 100){
+//     else if(age > 18 && ifInput.value < 100){
 //         mainText.textContent = `You're an adult. Wanna start a business?`;
 //     }
 //     else {
